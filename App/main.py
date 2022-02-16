3d161ba12e05bd50defa05fa05c5b318b7963393
limmireu <alfmalfm1214@gmail.com>
2022-02-16 17:28:10 +0000

    upload

--- App/main.py
+++ App/main.py
@@ -10,6 +10,9 @@ user_data = json.load(userinfo)
 user_name = user_data["name"]
 email = user_data["email"]
 github_url = user_data["github-url"]
+projects = user_data['projects']
+langs = user_data['langs']
+career = user_data['Career']
 
 text = """
 ------------------------------\n
@@ -27,31 +30,14 @@ def main_pages():
             email=email,
              
             github_url=github_url, 
-            
-            dply_lang=user_data["projects"]["dply"]["lang"], 
-            dply_info=user_data["projects"]["dply"]["info_text"], 
-            dply_github=user_data["projects"]["dply"]["github"],
 
-            frontera_lang=user_data["projects"]["frontera"]["lang"], 
-            frontera_info=user_data["projects"]["frontera"]["info_text"], 
-            frontera_github=user_data["projects"]["frontera"]["github"],
+            langs = langs,
+            projects = projects,
 
-            yuna_lang=user_data["projects"]["yuna"]["lang"], 
-            yuna_info=user_data["projects"]["yuna"]["info_text"], 
-            yuna_github=user_data["projects"]["yuna"]["github"],
-
-            hana_lang=user_data["projects"]["hana"]["lang"],
-            hana_info=user_data["projects"]["hana"]["info_text"],
-            hana_github=user_data["projects"]["hana"]["github"],
-
-            mina_lang=user_data["projects"]["mina"]["lang"],
-            mina_info=user_data["projects"]["mina"]["info_text"],
-            mina_github=user_data["projects"]["mina"]["github"],
-
-            team_list=user_data["Career"]["team"],
+            career=career
         )
     else:
         return "Incorrect connection method"
 
 if __name__ == "__main__":
-    service.run(host="0.0.0.0", port="1234", debug=True)
+    service.run(host="0.0.0.0", port="12347", debug=True)
